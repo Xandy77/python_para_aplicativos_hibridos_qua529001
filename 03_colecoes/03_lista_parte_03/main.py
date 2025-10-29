e54be023cfe6d60729053be8a5fe013a3eb11e31
valdomiropereira <xandnho2017@gmail.com>
2025-10-29 16:58:35 -0300

    Aula do dia 29/10/2025

---
+++ 03_colecoes/03_lista_parte_03/main.py
@@ -0,0 +1,51 @@
+# biblioteca
+import os
+
+
+# declaração de lista
+nomes = []
+
+# limpa console
+os.system("cls")
+
+# loop
+while True:
+    # menu
+    print("1 - Inserir novo nome")
+    print("2 - Exibir lista de nomes")
+    print("3 - Excluir nome na lista")
+    print("4 - Sair do programa")
+    opcao = input("Informe a opção desejada: ").strip()
+
+    match opcao:
+        case "1":
+            os.system("cls")
+            novo_nome = input("Informe um novo nome: ").strip().title()
+            nomes.append(novo_nome) # insere novo nome na lista
+            os.system("cls")
+            print(f"{novo_nome} cadastrado com sucesso")
+        case "2":
+            os.system("cls")
+            print("Lista de nomes:\n")
+            for i in range(len(nomes)): # FIXME 
+                print(f"{i} - {nomes[i]}") # estudar mais sobre essa linha
+            print(f"\n{'-'*40}\n")
+            continue
+        case "3":
+            os.system("cls")
+            try:
+                posicao = int(input("Informe a posição a ser excluída: ").strip())
+                if posicao >= 0 and posicao < len(nomes):
+                    del(nomes[posicao])
+                    print("Nome excluído com sucesso.")
+                else:
+                    print("Posição inexistente.")
+            except Exception as e:
+                print(f"Posição inválida. {e}.")
+            continue
+        case "4":
+            print("Programa encerrado.")
+            break
+        case _:
+            print("Opção inválida.")
+            continue
---
+++ 03_colecoes/04_tupla/main.py
@@ -0,0 +1,16 @@
+# Tupla: É uma lista imutável, não pode inserir, ordenar, excluir. Na tupla têm-se parenteses
+# declaração de tupla
+
+dias_semana = (
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira", 
+    "Quarta-feira", 
+    "Quinta feira", 
+    "Sexta-feira", 
+    "Sábado"
+)
+
+# saída de dados
+for dia in dias_semana:
+    print(dia)
